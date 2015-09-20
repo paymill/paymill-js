64928694ebc3af179879637736edcb0b9185c728
Vlad Barboni <vlad.barboni@gmail.com>
2015-09-20 19:54:14 +0200

    Add client id, checksum action and reusable payment with description to checksum creator.

--- test/checksum_service.js
+++ test/checksum_service.js
@@ -30,6 +30,9 @@ describe('ChecksumService', function() {
 			var amount = shared.randomAmount();
 			var return_url = "http://www." + shared.randomDescription() + ".com";
 			var cancel_url = "http://www." + shared.randomDescription() + ".com";
+			var checksum_action = 'payment';
+			var require_reusable_payment = true;
+			var reusable_payment_description = shared.randomDescription();
 			var billing = new pm.Address();
 			billing.postal_code = shared.randomAmount();
 			var shipping = new pm.Address();
@@ -39,15 +42,18 @@ describe('ChecksumService', function() {
 			items[1] = new pm.ShoppingCartItem();
 			items[0].name = shared.randomDescription();
 			items[1].name = shared.randomDescription();
-			pmc.checksums.forPaypal(amount, shared.currency, return_url, cancel_url).withBillingAddress(billing).withShippingAddress(shipping).withDescription(description).withItems(items).create().then(function(result) {
+			pmc.checksums.forPaypal(amount, shared.currency, return_url, cancel_url).withBillingAddress(billing).withShippingAddress(shipping).withDescription(description).withItems(items).withChecksumAction(checksum_action).withReusablePayment(require_reusable_payment, reusable_payment_description).create().then(function(result) {
 				expect(result).to.be.a(pm.Checksum);
 				expect(result.checksum).not.to.be.empty;
+				expect(result.action).not.to.be.empty;
 				expect(result.data).to.have.string(amount);
 				expect(result.data).to.have.string(encodeURIComponent(return_url));
 				expect(result.data).to.have.string(encodeURIComponent(cancel_url));
 				expect(result.data).to.have.string(encodeURIComponent(description));
 				expect(result.data).to.have.string(encodeURIComponent(items[0].name));
 				expect(result.data).to.have.string(encodeURIComponent(items[1].name));
+				expect(result.data).to.have.string(encodeURIComponent(require_reusable_payment));
+				expect(result.data).to.have.string(encodeURIComponent(reusable_payment_description));
 			}).then(function() {
 				done();
 			}, function(err) {
